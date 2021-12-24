e404ce6fea3e8cde934f0c2f26663ee5ab0cffcd
Michael Loftus <mjloftus93@gmail.com>
2021-12-24 13:58:26 -0500

    add vector test fixture

--- test/test_vector.c
+++ test/test_vector.c
@@ -2,9 +2,25 @@
 #include "../vector.h"
 
 /*
+ * Test Fixtures
+ */
+Vector* good_vector;
+
+void setup_good_vector(void) {
+	good_vector = vector_create(sizeof(int));
+	for (int i = 0; i < 10; ++i) {
+		vector_push(good_vector, &i);
+	}
+}
+
+void teardown_good_vector(void) {
+	vector_delete(good_vector);
+}
+
+/*
  * vector_create
  */
-START_TEST (create_returns_vector_on_success) {
+START_TEST (create_returns_vector_when_success) {
 	Vector* v = vector_create(4);
 	ck_assert_ptr_nonnull(v);
 	vector_delete(v);
@@ -13,27 +29,17 @@ START_TEST (create_returns_vector_on_success) {
 /*
  * vector_get
  */
-START_TEST (get_returns_elem_in_range) {
-	Vector* v = vector_create(sizeof(int));
+START_TEST (get_returns_elem_when_in_range) {
 	for (int i = 0; i < 10; ++i) {
-		vector_push(v, &i);
+		ck_assert_int_eq(*((int*)vector_get(good_vector, i)), i);
 	}
-	for (int i = 0; i < 10; ++i) {
-		ck_assert_int_eq(*((int*)vector_get(v, i)), i);
-	}
-	vector_delete(v);
 } END_TEST
 
 
-START_TEST (get_returns_null_out_of_range) {
-	Vector* v = vector_create(sizeof(int));
-	for (int i = 0; i < 10; ++i) {
-		vector_push(v, &i);
-	}
+START_TEST (get_returns_null_when_out_of_range) {
 	for (int i = 10; i < 20; ++i) {
-		ck_assert_ptr_null(vector_get(v, i));
+		ck_assert_ptr_null(vector_get(good_vector, i));
 	}
-	vector_delete(v);
 } END_TEST
 
 START_TEST (get_returns_null_when_no_vector) {
@@ -44,38 +50,23 @@ START_TEST (get_returns_null_when_no_vector) {
  * vector_in
  */
 START_TEST (in_returns_elem_when_found) {
-	Vector* v = vector_create(sizeof(int));
 	for (int i = 0; i < 10; ++i) {
-		vector_push(v, &i);
+		ck_assert_int_eq(*((int*)vector_in(good_vector, &i)), i);
 	}
-	for (int i = 0; i < 10; ++i) {
-		ck_assert_int_eq(*((int*)vector_in(v, &i)), i);
-	}
-	vector_delete(v);
 } END_TEST
 
 
 START_TEST (in_returns_null_when_not_found) {
-	Vector* v = vector_create(sizeof(int));
-	for (int i = 0; i < 10; ++i) {
-		vector_push(v, &i);
-	}
 	for (int i = 10; i < 20; ++i) {
-		ck_assert_ptr_null(vector_in(v, &i));
+		ck_assert_ptr_null(vector_in(good_vector, &i));
 	}
-	vector_delete(v);
 } END_TEST
 
 /*
  * vector_length
  */
 START_TEST (length_returns_size_of_vector) {
-	Vector* v = vector_create(sizeof(int));
-	for (int i = 0; i < 10; ++i) {
-		vector_push(v, &i);
-	}
-	ck_assert_int_eq(vector_length(v), 10);
-	vector_delete(v);
+	ck_assert_int_eq(vector_length(good_vector), 10);
 } END_TEST
 
 Suite* vector_suite(void) {
@@ -83,21 +74,24 @@ Suite* vector_suite(void) {
 	TCase* tc_create;
 	s = suite_create("Vector");
 	tc_create = tcase_create("create");
-	tcase_add_test(tc_create, create_returns_vector_on_success);
+	tcase_add_test(tc_create, create_returns_vector_when_success);
 	suite_add_tcase(s, tc_create);
 	TCase* tc_get;
 	tc_get = tcase_create("get");
-	tcase_add_test(tc_get, get_returns_elem_in_range);
-	tcase_add_test(tc_get, get_returns_null_out_of_range);
+	tcase_add_checked_fixture(tc_get, setup_good_vector, teardown_good_vector);
+	tcase_add_test(tc_get, get_returns_elem_when_in_range);
+	tcase_add_test(tc_get, get_returns_null_when_out_of_range);
 	tcase_add_test(tc_get, get_returns_null_when_no_vector);
 	suite_add_tcase(s, tc_get);
 	TCase* tc_in;
 	tc_in = tcase_create("in");
+	tcase_add_checked_fixture(tc_in, setup_good_vector, teardown_good_vector);
 	tcase_add_test(tc_in, in_returns_elem_when_found);
 	tcase_add_test(tc_in, in_returns_null_when_not_found);
 	suite_add_tcase(s, tc_in);
 	TCase* tc_length;
 	tc_length = tcase_create("length");
+	tcase_add_checked_fixture(tc_length, setup_good_vector, teardown_good_vector);
 	tcase_add_test(tc_length, length_returns_size_of_vector);
 	suite_add_tcase(s, tc_length);
 
