a0fcfb4b2766107fb57241d50032603f3c9f0cd3
Michael Loftus <mjloftus93@gmail.com>
2021-12-24 00:57:46 -0500

    add tests for negative and zero create size args

--- test/test_vector.c
+++ test/test_vector.c
@@ -1,18 +1,33 @@
 #include <check.h>
 #include "../vector.h"
 
-START_TEST (test_vector_create) {
-	vector* v = malloc(sizeof(vector));
+START_TEST (vector_is_created_with_positive_elem_size) {
+	Vector* v = vector_create(4);
 	ck_assert_ptr_nonnull(v);
+	vector_delete(v);
+} END_TEST
+
+START_TEST (vector_is_not_created_with_zero_elem_size) {
+	Vector* v = vector_create(0);
+	ck_assert_ptr_null(v);
+	vector_delete(v);
+} END_TEST
+
+START_TEST (vector_is_not_created_with_negative_elem_size) {
+	Vector* v = vector_create(-1);
+	ck_assert_ptr_null(v);
+	vector_delete(v);
 } END_TEST
 
 Suite* vector_suite(void) {
 	Suite* s;
-	TCase* tc_core;
+	TCase* tc_create;
 	s = suite_create("Vector");
-	tc_core = tcase_create("Core");
-	tcase_add_test(tc_core, test_vector_create);
-	suite_add_tcase(s, tc_core);
+	tc_create = tcase_create("create");
+	tcase_add_test(tc_create, vector_is_created_with_positive_elem_size);
+	tcase_add_test(tc_create, vector_is_not_created_with_zero_elem_size);
+	tcase_add_test(tc_create, vector_is_not_created_with_negative_elem_size);
+	suite_add_tcase(s, tc_create);
 
 	return s;
 }
