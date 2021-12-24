1bbb35704aee8a80dc1df775e4f96f90d99eb8e5
Michael Loftus <mjloftus93@gmail.com>
2021-12-24 02:29:41 -0500

    add tests for in

--- test/test_vector.c
+++ test/test_vector.c
@@ -36,6 +36,32 @@ START_TEST (get_returns_null_out_of_range) {
 	vector_delete(v);
 } END_TEST
 
+/*
+ * vector_in
+ */
+START_TEST (in_returns_elem_when_found) {
+	Vector* v = vector_create(sizeof(int));
+	for (int i = 0; i < 10; ++i) {
+		vector_push(v, &i);
+	}
+	for (int i = 0; i < 10; ++i) {
+		ck_assert_int_eq(*((int*)vector_in(v, &i)), i);
+	}
+	vector_delete(v);
+} END_TEST
+
+
+START_TEST (in_returns_null_when_not_found) {
+	Vector* v = vector_create(sizeof(int));
+	for (int i = 0; i < 10; ++i) {
+		vector_push(v, &i);
+	}
+	for (int i = 10; i < 20; ++i) {
+		ck_assert_ptr_null(vector_in(v, &i));
+	}
+	vector_delete(v);
+} END_TEST
+
 Suite* vector_suite(void) {
 	Suite* s;
 	TCase* tc_create;
@@ -48,6 +74,11 @@ Suite* vector_suite(void) {
 	tcase_add_test(tc_get, get_returns_elem_in_range);
 	tcase_add_test(tc_get, get_returns_null_out_of_range);
 	suite_add_tcase(s, tc_get);
+	TCase* tc_in;
+	tc_in = tcase_create("in");
+	tcase_add_test(tc_in, in_returns_elem_when_found);
+	tcase_add_test(tc_in, in_returns_null_when_not_found);
+	suite_add_tcase(s, tc_in);
 
 	return s;
 }
